0d0276af82ca428e4a1ba12d6b132db2990185c5
Ourodjowd <ourodjowd3@gmail.com>
2024-05-05 22:50:01 +0530

    start server modified

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000; // Use the port provided by Render or default to 3000
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,7 +11,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// // Start the server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+// If the script is executed directly (not as a module), start the server
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000; // Use the port provided by Render or default to 3000
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
