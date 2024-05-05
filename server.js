4d28420d97b23432f69ff4aa6e5affd0b75b98ca
Ourodjowd <ourodjowd3@gmail.com>
2024-05-05 22:41:35 +0530

    removing a loop

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
